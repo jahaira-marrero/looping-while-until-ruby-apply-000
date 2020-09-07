5f3980fdf32317da0a93fdecf52e61cb77d39bb7
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-07 16:10:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,6 +4,12 @@ def using_while
 	   levitation_force += 1
 	puts "Wingardium Leviosa #{levitation_force}"
 	end
+	
+	levitation_force = 6
+	until levitation_force == 20
+	 puts "Wingardium Leviosa is less than 20."
+	 levitation_force += 10
+	end
   #your code here
 end
 
